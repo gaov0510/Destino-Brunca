c1e14db93f526d5990ee430e907570ce42ace494
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2025-06-14 16:38:16 -0300

    fix: expo-doctor

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -14,6 +14,7 @@ import IconNews from "@/components/Icons/IconNotice";
 import IconConfig from "@/components/Icons/IconConfig";
 import IconSearch from "@/components/Icons/IconSearch";
 import { useAppConfig } from "@/hooks/useConfig";
+import { useTranslation } from "react-i18next";
 
 interface Tab {
   route: Href;
@@ -47,7 +48,7 @@ const tabs: Tab[] = [
 export default function CustomLayout() {
   const router = useRouter();
   const pathname = usePathname();
-  const config = useAppConfig();
+  const { t } = useTranslation();
 
   const getIsActive = (partialRoute: string) => {
     console.log(pathname, partialRoute, pathname === partialRoute);
@@ -77,7 +78,7 @@ export default function CustomLayout() {
             onPress={() => router.push("/(screens)/config")}
           >
             <Text style={styles.buttonText}>
-              {config.config.language.toLocaleUpperCase()} {">"}
+              {t("lenguage").toLocaleUpperCase()} {">"}
             </Text>
           </TouchableOpacity>
         ) : (
